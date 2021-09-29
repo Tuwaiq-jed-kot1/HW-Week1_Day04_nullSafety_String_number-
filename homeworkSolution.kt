d3fc8ee83919ebcb36078c890241b13c6f7a5849
Fatim <Fatimah.Alsuhaymi.jed.@tuwaiq.edu.sa>
2021-09-30 01:44:25 +0300

    i cannot understand the qustion but i still trying

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,3 +1,14 @@
-fun main(){
-
+fun main (args:Array<String>) {
+  println(getlength('This is a string!'))
 }
+fun getlength(str:String) :Int{
+
+  if(str == null){
+    return 0
+  }
+  else{
+    return  str.length
+  }
+}
+
+part2//
