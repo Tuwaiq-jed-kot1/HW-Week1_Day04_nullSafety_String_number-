fd2b9d7e776654183a51095342d5c5ad14cbd18f
Renad Omar <reomqu@gmail.com>
2021-09-30 00:46:50 +0300

    Week1 Day4 H.W Solution

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,3 +1,40 @@
+//Part|
 fun main(){
+    fun getLength(str: String?) : Int {
+        val l = if (str != null) str.length else 0
+        }//null check
 
+        return str.length;
+    }
+
+    fun main(args: Array<String>) {
+        println(getLength('This is a string!'))
+    }
 }
+//end Part|
+
+
+
+
+//Part||
+fun main(){
+    val str = "The quick brown fox jumps over the lazy dog"
+    val str2 = str.substring(str.indexOf("brown"),str.indexOf("over"))
+    println(str2)
+}
+//end Part||
+
+
+
+
+//Part|||
+fun main() {
+    val f: Double? = "212".toDoubleOrNull()
+    try {
+        val c = ((5 * (f!! - 32)) / 9)
+        println("$f degree Fahrenheit is equal to $c in Celsius.")
+    } catch (e: Exception) {
+        println("Your error is: $e")
+    }
+}
+//end Part|||
