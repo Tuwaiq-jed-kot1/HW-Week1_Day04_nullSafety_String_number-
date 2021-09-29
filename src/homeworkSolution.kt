c9e6be4bf2b776baabf76978488cb9762ac7dade
HaniDabash <hani.dabsh.jed.k1@tuwaiq.edu.sa>
2021-09-29 21:51:03 +0300

    HW_Week1_Day4_StringsAndNumbers 😃

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,5 +1,6 @@
 // HW_Week1_Day4_StringsAndNumbers 😃 ..
 
+
 fun main(args: Array<String>) {
     println(getLength("This is a string!"))
     println("-----------------------------------------------------")
