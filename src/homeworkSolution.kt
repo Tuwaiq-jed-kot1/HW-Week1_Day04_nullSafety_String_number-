37ec3c311c5cba3cbaf865713385a5587bd5eb7e
AlmohaimeedAbdullah <abdullah.almouhamid.jed.k1@tuwaiq.edu.sa>
2021-09-29 21:48:24 +0300

    nice

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,3 +1,27 @@
-fun main(){
+//      ----->>>> Part 1 <<<<-----
+fun getLength(str: String?) {
+    if (str == null ){
+        println("Zero")
+    }else {
+        println(str.length)
+    }
+}
+fun main(args: Array<String>) {
+    getLength("This is a string!")
+}
 
+//      ----->>>> Part 2 <<<<-----
+fun main(){
+    var str = "The quick brown fox jumps over the lazy dog"
+    println(str.substring(9,25))
+}
+
+//      ----->>>> Part 3 <<<<-----
+fun main() {
+    var F  = 212.0
+    if (F == null){
+        println("i can't do the math:(")
+    }
+    var c = (5 * (F - 32))/9
+    println("$F degree Fahrenheit is equal to $c in Celsius")
 }
