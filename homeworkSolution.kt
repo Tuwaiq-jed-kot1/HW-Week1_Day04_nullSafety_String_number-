983dccfc6adce7662767ff60ca75357a5f0c8054
Bashayer Alosaimi <bashayer.alosaimi37@gmail.com>
2021-09-29 22:35:12 +0300

    I like it

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,3 +1,30 @@
-fun main(){
 
+fun main(args: Array<String>) {
+// Part 1 - Call getLength
+    println(getLength("This is a string!"))
+
+
+// Part 2 substring
+    val old = "The quick brown fox jumps over the lazy dog."
+    var new = old.substring(9,25)
+    println(new)
+
+//Part 3 Numbers
+
+    val fahrenheit:Double? =212.0
+    val celsius:Double
+
+    if (fahrenheit != null){
+        celsius =(((fahrenheit - 32) * 5) / 9)
+        println(" ${"%.2f".format(fahrenheit)} degree Fahrenheit is equal to ${"%.2f".format(celsius)} in Celsius")
+    }
+    else println("The program can't convert to Celsius because fahrenheit is null")
+
+}
+
+//------------------ function ----------------------
+
+// check nullablety and return length of str or -1 if str is null
+fun getLength(str: String?) : Int {
+    if (str != null) return str.length else return -1
 }
