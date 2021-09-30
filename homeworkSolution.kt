a77a730eabc1cead5fc46ea6f7712445de4dca7b
AfnanAlmohammdii <91476967+AfnanAlmohammdii@users.noreply.github.com>
2021-09-30 04:47:32 +0300

    ok , i would like to thank Kotlin to help and
     suggestion idea to soliving first question

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,3 +1,39 @@
+
 fun main(){
+/// quesstion 1////////
+    fun getLength(str: String?):Int {
+        return if (str == null) 0 else str.toInt()
+    }
+    println("the length is ${getLength("3870")} ")
+    fun main(args: Array<String>) {
+        println(getLength("This is a string!"))
+    }
+
+    /////// quesstion 2///////////
+
+    fun main() {
+
+        var old = " The quick brown fox jumps over the lazy dog"
+        var new = old.length
+
+        println(old.subSequence(10, 27))
+    }
+
+///////  /////// quesstion 3///////////
+
+    fun main(){
+
+        var Fahrenheit :Int? =212
+        var Celsius : Double =(5 * (Fahrenheit?.toDouble()!! -32) )/9
+
+        println("the degree in Celsius is $Celsius so you will die soon")
+
+    }
+
+
+
+
+
+
 
 }
